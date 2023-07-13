f423adc289c80c9e9c88d5f36e72e92ca7f5cc11
Sanjeev Choudhary <sanjeev.choudhary@adisan.in>
2023-07-13 09:41:34 +0530

    Add project files.

---
+++ DatatablesCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatatablesCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
